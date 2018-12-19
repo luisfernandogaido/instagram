f4429631d4b92df2619f5f95ccb9f8c141bcd8f2
Luis Fernando Gaido <luisfernandogaido@gmail.com>
2018-12-19 00:18:45 -0200

    Baixando imagens e vídeos de todos de perfis de um arquivo.

--- main.go
+++ main.go
@@ -1,21 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
+)
+
+const (
+	dirname = "./data"
 )
 
 func main() {
-	perfil, err := PerfilCarrega("https://www.instagram.com/mongodb")
+	usernames := make(map[string]bool)
+	b, err := ioutil.ReadFile("./in.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, n := range perfil.Nodes {
-		post, err := PostCarrega(n.Shortcode)
+	links := strings.Split(string(b), "\r\n")
+	out, err := os.Create("./out.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	os.Mkdir(dirname, 0755)
+	for i, link := range links {
+		index := strings.Index(link, "?")
+		if index == -1 {
+			index = len(link)
+		}
+		link = strings.Replace(link[0:index], "//instagram", "//www.instagram", 1)
+		if usernames[link] {
+			continue
+		}
+		usernames[link] = true
+		fmt.Printf("Perfil: %v. %v de %v.\n", link, i+1, len(links))
+		out.WriteString(link + "\r\n")
+		perfil, err := PerfilCarrega(link)
+		if _, ok := usernames[perfil.UserName]; ok {
+			continue
+		}
+		usernames[perfil.UserName] = true
 		if err != nil {
 			log.Fatal(err)
 		}
-		if err := post.Download("D:\\Users\\81092610\\Desktop\\posts"); err != nil {
-			log.Fatal(err)
+		fmt.Printf("Username: %v, nodes: %v\n", perfil.UserName, len(perfil.Nodes))
+		for i, node := range perfil.Nodes {
+			fmt.Println(i, node.Shortcode)
+			post, err := PostCarrega(node.Shortcode)
+			if err != nil {
+				log.Fatal(err)
+			}
+			post.Download(dirname)
 		}
 	}
 }
